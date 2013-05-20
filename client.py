d1585a4b288aa084f2a6d0b9b9c42ed9d381debc
Frankie <max00355@gmail.com>
2013-05-19 22:48:30 -0400

    First Commit

---
+++ client.py
@@ -0,0 +1,36 @@
+import socket
+import json
+import threading
+import random
+
+class CentralClient:
+    def __init__(self, room, nick):
+        self.ip = ""
+        self.port = 5124
+        self.nick = nick
+        self.sock = socket.socket()
+        self.room = room
+        self.id = str(random.random())
+
+    def main(self):
+        self.sock.connect((self.ip, self.port))
+        threading.Thread(target=self.listen).start()
+        while True:
+            msg = raw_input(self.nick+": ")
+            msg = json.dumps({"id":self.id,"nick":self.nick, "room":self.room, "msg":msg})
+            self.sock.send(msg)
+
+    def listen(self):
+        while True:
+            data = self.sock.recv(1024)
+            if data:
+                try:
+                    data = json.loads(data)
+                except:
+                    continue
+                if data['room'] == self.room and data['id'] != self.id:
+                    print data['nick']+": "+data['msg']
+if __name__ == "__main__":
+    nick = raw_input("Nick: ")
+    room = raw_input("Room: ")
+    CentralClient(room, nick).main()
---
+++ server.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+
+class CentralServer:
+    def __init__(self):
+        self.nodes = []
+        self.sock = socket.socket()
+    def main(self):
+        self.sock.bind(("0.0.0.0", 5124))
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.listen(5)
+        while True:
+            obj, conn = self.sock.accept()
+            self.nodes.append(obj)
+            threading.Thread(target=self.handle, args=(obj,)).start()
+    
+    def handle(self, obj):
+        while True:
+            data = obj.recv(1024)
+            if not data:
+                break
+            else:
+                for x in self.nodes:
+                    x.send(data)
+if __name__ == "__main__":
+    CentralServer().main()
